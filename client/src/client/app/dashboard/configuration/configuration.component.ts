243bb60a90b2dbb5908ba7f07f206d1896102634
hazem1193 <hazem.mamdouh.kotb@gmail.com>
2017-01-27 19:05:31 +0200

    configuration page

--- client/src/client/app/dashboard/configuration/configuration.component.ts
+++ client/src/client/app/dashboard/configuration/configuration.component.ts
@@ -6,4 +6,48 @@ import { Component } from '@angular/core';
     templateUrl: './configuration.component.html'
 })
 
-export class ConfigurationComponent { }
+export class ConfigurationComponent { 
+
+    public childItems: any[] = [{
+			    show:true
+			   }];
+    public childstemp: any[];
+    private size: number = this.childItems.length;
+
+    public createTab(index: number){
+
+    	if(index == this.childItems.length -1){
+
+			this.childstemp = [];
+			for (let item of this.childItems) {
+			    this.childstemp.push(Object.assign({},item));
+			}
+
+			this.childItems = [];
+			for (var i = 0; i < this.childstemp.length-1; i++) {
+				let item: any = this.childstemp[i];
+				item.active = false;
+			    this.childItems.push(item);
+			}
+
+			this.childItems.push({name: 'Data ' + this.size,
+				    urlTpe: 'type' + this.size,
+				    urlType: 'urlString',
+				    act:true,
+				    act2:false,
+				    active:true,
+				    show:false
+				   });
+
+		    this.size ++;
+
+			this.childItems.push({
+				    show:true
+				   });
+		}
+    }
+
+    clicked(index: number) {
+		this.childItems.splice(index, 1);
+  	}
+}
--- client/src/client/app/dashboard/configuration/configuration.module.ts
+++ client/src/client/app/dashboard/configuration/configuration.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { ConfigurationComponent } from './configuration.component';
 
+import {
+		TabsModule
+	} from 'ng2-bootstrap/ng2-bootstrap';
+
 @NgModule({
-    imports: [],
+    imports: [
+		TabsModule,
+		FormsModule,
+		CommonModule
+	     ],
     declarations: [ConfigurationComponent],
     exports: [ConfigurationComponent]
 })
