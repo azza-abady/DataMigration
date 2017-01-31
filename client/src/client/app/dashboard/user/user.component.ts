1b0702f2285103c192b938faef3342762e1b1669
azza_hamdy <azza_hamdy@siliconexpert.com>
2017-01-31 18:30:53 +0200

    binding of user form

--- client/src/client/app/dashboard/user/user.component.ts
+++ client/src/client/app/dashboard/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
 	moduleId: module.id,
@@ -6,6 +7,16 @@ import { Component } from '@angular/core';
     templateUrl: './user.component.html'
 })
 
-export class UserComponent { }
+export class UserComponent {
+roles = ['Admin', 'user','Admin2','user2'];
 
+model = new User(1, 'azza', 'azza', this.roles[0]);
 
+submitted = false;
+
+onSubmit() { this.submitted = true; }
+
+// TODO: Remove this when we're done
+get diagnostic() { return JSON.stringify(this.model);
+}
+}
--- client/src/client/app/dashboard/user/user.module.ts
+++ client/src/client/app/dashboard/user/user.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
 import { UserComponent } from './user.component';
 
 @NgModule({
     imports: [
-		RouterModule
+		RouterModule,
+    BrowserModule,
+    FormsModule
 	],
     declarations: [UserComponent],
+    bootstrap: [UserComponent],
     exports: [UserComponent]
 })
 
