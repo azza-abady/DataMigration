c29ec2e805465459c3e22a9b62912fcde0391717
hazem1193 <hazem.mamdouh.kotb@gmail.com>
2017-03-04 15:05:03 +0200

    GUI Dashboard

--- client/src/client/app/dashboard/home/home.module.ts
+++ client/src/client/app/dashboard/home/home.module.ts
@@ -3,12 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { CarouselModule, DropdownModule, AlertModule } from 'ng2-bootstrap/ng2-bootstrap';
 
-import { TimelineComponent, ChatComponent, NotificationComponent } from './home.component';
+import { TimelineComponent, NotificationComponent } from './home.component';
 
 @NgModule({
     imports: [CommonModule, CarouselModule, DropdownModule, AlertModule],
-    declarations: [HomeComponent, TimelineComponent, ChatComponent, NotificationComponent],
-    exports: [HomeComponent, TimelineComponent, ChatComponent, NotificationComponent]
+    declarations: [HomeComponent, TimelineComponent, NotificationComponent],
+    exports: [HomeComponent, TimelineComponent, NotificationComponent]
 })
 
 export class HomeModule { }
